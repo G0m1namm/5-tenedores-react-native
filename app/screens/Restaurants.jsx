b974034420573f56ceaa4805a4c22527c1b48826
jherson_martelo <jherson.martelo@pragma.com.co>
2020-08-19 17:51:38 -0500

    creando stack de navegacion

--- app/screens/Restaurants.jsx
+++ app/screens/Restaurants.jsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function Restaurants() {
     return (
         <View>
-            <Text></Text>
+            <Text>Restaurantes</Text>
         </View>
     )
 }
