557a614512c5e4dc6db2095ac05eb187a4489c54
jherson_martelo <jherson.martelo@pragma.com.co>
2020-08-20 17:47:26 -0500

    assets añadidos, agregando vista de login

--- app/Navigation/AccountStack.jsx
+++ app/Navigation/AccountStack.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Account from '../screens/Account/Account';
+import Login from '../screens/Account/Login';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,13 @@ export default function AccountStack() {
                 component={Account}
                 options={{
                     title: "Perfil"
+                }}
+            />
+            <Stack.Screen
+                name="login"
+                component={Login}
+                options={{
+                    title: "Iniciar sesión"
                 }}
             />
         </Stack.Navigator>
---
+++ app/screens/Account/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+function Login() {
+    return (
+        <View>
+            <Text>Iniciar sesion</Text>
+        </View>
+    )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
